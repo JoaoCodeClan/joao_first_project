db9ecd3efac4f194ef7ff78104c9f25b04447b0c
Joao <johnynecas@hotmail.com>
2017-10-24 10:40:17 +0100

    created basic merchant index

--- controllers/merchants_controller.rb
+++ controllers/merchants_controller.rb
@@ -6,5 +6,20 @@ require_relative('../models/transaction.rb')
 
 
 get '/merchants' do
-  erb( :index_merchants)
+  erb( :"merchants/index_merchants")
+end
+
+get '/merchants/all' do
+  @merchants = Merchant.all()
+  erb(:"merchants/all_merchants")
+end
+
+get '/merchants/new' do
+  erb(:"merchants/new_merchant")
+end
+
+post '/merchants/all' do
+  @merchant = Merchant.new(params)
+  @merchant.save()
+  erb(:"merchants/new_merchant_report")
 end
